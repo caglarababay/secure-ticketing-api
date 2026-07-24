d142d45c217e0eecca4b06b5d4e105d438c3b3a0
cababay <caglar.ababay@gmail.com>
2026-07-25 00:54:35 +0300

    Add initial Spring Boot project structure

---
+++ src/main/java/com/caglar/secure_ticketing_api/SecureTicketingApiApplication.java
@@ -0,0 +1,13 @@
+package com.caglar.secure_ticketing_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTicketingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTicketingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caglar/secure_ticketing_api/SecureTicketingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caglar.secure_ticketing_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTicketingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
